e58c0e5bd394e04b71f195de8c94cf016696e194
Ricardo <ricardo.detoni@gmail.com>
2024-01-02 17:56:29 -0300

    | Adds script to automate deploy on raspberry pi server

--- deploy_to_pi.sh
+++ deploy_to_pi.sh
@@ -1,39 +1,33 @@
 #!/bin/bash
-
-# Get the directory where the script is located
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
+set -x
 # Set your remote server's SSH details
-SSH_USER="your-ssh-username"
-SSH_HOST="your-remote-server-ip"
-SSH_PASSWORD="your-ssh-password"
+SSH_USER="admin"
+SSH_HOST="192.168.0.43"
 
-# Set your Docker image details
-IMAGE_NAME="your-docker-image-name"
-DOCKERFILE_PATH="$SCRIPT_DIR/Dockerfile"
+#delete folders that are not going to be used
+rm -r dashboard/build
+rm -r dashboard/node_modules
 
-# Convert Windows path to WSL-compatible path if running on Windows
-if [[ $(uname -s) == MINGW* || $(uname -s) == CYGWIN* ]]; then
-  DOCKERFILE_PATH=$(wslpath -u "$DOCKERFILE_PATH")
-fi
+#Put the whole project on a tar file
+tar -zcvf dashboard.tar.gz dashboard
 
-# Build the Docker image
-docker build -t $IMAGE_NAME -f $DOCKERFILE_PATH .
-
-# Save the Docker image as a tar file
-docker save -o image.tar $IMAGE_NAME
-
-# Copy the image to the remote server
-sshpass -p "$SSH_PASSWORD" scp image.tar $SSH_USER@$SSH_HOST:/path/to/remote/location
+#Send project to PI
+scp dashboard.tar.gz $SSH_USER@$SSH_HOST:/home/admin/docker-images
 
 # SSH into the remote server
 ssh $SSH_USER@$SSH_HOST << EOF
-    # Load the Docker image
-    docker load -i /path/to/remote/location/image.tar
-
+	set -x
+	
+	# Extract project contents
+	tar -xvzf /home/admin/docker-images/dashboard.tar.gz -C /home/admin/docker-images/
+	
+    # Build the Docker image
+	cd /home/admin/docker-images/dashboard
+	docker build -t wallet_dashboard:latest -f Dockerfile .
+		
     # Optionally, push the image to a Docker registry
     # docker push your-registry-url/$IMAGE_NAME
 
     # Run your Docker container
-    docker run -d -p 3000:3000 $IMAGE_NAME
+    docker run -d -p 3000:3000 wallet_dashboard:latest
 EOF
